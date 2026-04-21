7d2c3649e9535304f76ef28a65534540f8cadc32
ductran0718 <duc.m.tran@sjsu.edu>
2026-04-21 13:15:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 const PORT = process.env.PORT || 3000;
 
-const GRID_WIDTH = 64;
-const GRID_HEIGHT = 48;
+const GRID_WIDTH = 96;
+const GRID_HEIGHT = 72;
 const DEFAULT_PIXEL = "#0b1220";
 
 app.use(express.static(path.join(__dirname, "public")));
